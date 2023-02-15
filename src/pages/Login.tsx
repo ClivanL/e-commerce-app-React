2aa408165c6bb6bf40da3c1b4e14054f30ad2fb8
Clivan Lee <clivan93@gmail.com>
2023-02-15 23:30:57 +0800

    added username and password for login and logout page, to tally with pre-seeded username and password data

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,38 @@
 import {useNavigate} from 'react-router-dom'
 import NavbarNoLogin from "../components/NavbarNoLogin";
+import users from '../../userAccounts.json'
 
 function Login({setLogin}:any){
+
     const navigate=useNavigate();
-    const handleLogin=()=>{
-        setLogin(true);
-        navigate("/")
+    const handleLogin=(event:any)=>{
+        event.preventDefault();
+        console.log(event.target.username.value);
+        const condition=users.filter((item)=>item.username===event.target.username.value && item.password===event.target.password.value)
+        console.log(condition)
+        if (condition.length!==0){
+            setLogin(true);
+            navigate("/");
+        }
+        else{
+            alert("Username or password is wrong. Please re-enter.")
+        }
+
+
     }
     return (<>
     <NavbarNoLogin/>
     <h1>Login/Logout page</h1>
     <div>
-    <button onClick={handleLogin}>Login</button>
-    </div>
+        <form onSubmit={handleLogin}>
+        <input id="username" placeholder="username"/>
+        <input id="password" placeholder="password" type="password" autoComplete='true'/>
+        <button>Login</button>
+        </form>
     
+    </div>
+    <p>Don't have an account?</p>
+    <button>Register</button>
     </>)
 }
 
