db98f134930f863c9e9d72565f0a7df038a12bda
Clivan Lee <clivan93@gmail.com>
2023-02-20 00:32:37 +0800

    css enhancements, login form css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,17 +25,46 @@ function Login(){
     }
     return (<>
     <NavbarNoLogin/>
-    <h1>Login/Logout page</h1>
-    <div>
-        <form onSubmit={handleLogin}>
-        <input id="username" placeholder="username"/>
-        <input id="password" placeholder="password" type="password" autoComplete='true'/>
-        <button>Login</button>
+    <form onSubmit={handleLogin}>
+    <div className="bg-grey-lighter min-h-screen flex flex-col">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 className="mb-8 text-3xl text-center">Login</h1>
+
+                    <input 
+                        id="username"
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="username"
+                        placeholder="Username" required/>                    
+
+                    <input 
+                        id="password"
+                        type="password"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" required autoComplete='true'/>
+                        
+                        <div>
+                        <button type="submit" 
+                        className="w-full text-center py-3 rounded bg-indigo-600 text-white hover:bg-indigo-400 focus:outline-none my-1">Login</button>
+                        </div>
+                    
+
+                    <div className="text-center text-sm text-grey-dark mt-4">
+                        Don't have an account? Click&nbsp;
+                        <a className="no-underline border-b border-grey-dark text-blue-600" onClick={()=>navigate("/signup")}>
+                            here
+                        </a> to&nbsp;
+                        <a className="no-underline border-b border-grey-dark text-blue-600" onClick={()=>navigate("/signup")}>
+                            register
+                        </a>
+                        .
+                    </div>
+                </div>
+            </div>
+        </div>
         </form>
-    
-    </div>
-    <p>Don't have an account?</p>
-    <button onClick={()=>navigate("/signup")}>Register</button>
     </>)
 }
 
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,15 +1,24 @@
 import {useState} from 'react'
+import Navbar from '../components/Navbar';
 
 function SignUp({addNewAccount}:any){
 
     const [register,setRegister]=useState({"fullname":"","username":"","email":"", "password":"", "confirmpassword":""});
 
     return <>
+    <Navbar/>
+    <form>
     <div className="bg-grey-lighter min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-
+                    <div className="text-grey-dark mt-6">
+                    Already have an account?&nbsp; 
+                    <a className="no-underline border-b border-blue text-blue-600" href="../login/">
+                        Log in
+                    </a>.
+                </div>
+                <br></br>
                     <input 
                         type="text"
                         className="block border border-grey-light w-full p-3 rounded mb-4"
@@ -66,24 +75,18 @@ function SignUp({addNewAccount}:any){
                     
 
                     <div className="text-center text-sm text-grey-dark mt-4">
-                        By signing up, you agree to the
+                        By signing up, you agree to the&nbsp; 
                         <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Terms of Service
-                        </a> and 
+                        </a> and&nbsp;  
                         <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Privacy Policy
                         </a>
                     </div>
                 </div>
-                <div className="text-grey-dark mt-6">
-                    Already have an account? 
-                    <a className="no-underline border-b border-blue text-blue" href="../login/">
-                        Log in
-                    </a>.
-                    
-                </div>
             </div>
         </div>
+        </form>
     </>
 }
 
