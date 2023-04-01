c5293651e0e729c2472a6208d8c91e9dea4868b7
clivan <clivan93@gmail.com>
2023-04-01 16:08:55 +0800

    added custom hook to retrieve purchase history, purchase history page shows data

--- src/pages/PurchaseHistory.tsx
+++ src/pages/PurchaseHistory.tsx
@@ -1,8 +1,50 @@
 import Navbar from "../components/Navbar";
+import useRetrievePurchaseHistory from "../hooks/useRetrievePurchaseHistory";
 
-export default function PurchaseHistory(){
-    return <>
-    <Navbar/>
-    Purchase History shown here
+interface Item {
+  id: number;
+  itemName: String;
+  price: number;
+  description: String;
+  imageUrl: String;
+  category: String;
+  ownerId: number;
+  quantity: number;
+}
+
+interface Cart {
+  id: number;
+  userId: number;
+  itemId: number;
+  quantity: number;
+  item: Item;
+}
+
+export default function PurchaseHistory() {
+  const purchaseHistory = useRetrievePurchaseHistory()!;
+
+  return (
+    <>
+      <Navbar />
+      Purchase History shown here
+      <table>
+        <tbody>
+          <tr>
+            <th>Item Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Purchased Date</th>
+          </tr>
+          {purchaseHistory?.map((element: Cart) => {
+            return <tr key={element.item.quantity}>
+              <td>{element.item.itemName}</td>
+              <td>{element.item.price}</td>
+              <td>{element.quantity}</td>
+              <td>To be added</td>
+            </tr>;
+          })}
+        </tbody>
+      </table>
     </>
+  );
 }
